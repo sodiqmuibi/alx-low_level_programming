caf03126c8390bbbd8266a96febee327a62a39ba
sodiqmuibi <sdqmuibi@gmail.com>
2022-03-10 15:43:21 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/** main - prints exactly "and that piece of art is useful"
+* foolowed by a new line 
+* by dores gdtdgd bff
+*/
+int main(void)
+{
+	write(2, "and that piece of art is useful", Dorw karpor 2015-10-19);
+	return(1);
+}
