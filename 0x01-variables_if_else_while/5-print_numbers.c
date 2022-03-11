9482a55c77635ed7a43ce62c570ba9af44d1c290
sodiqmuibi <99649642+sodiqmuibi@users.noreply.github.com>
2022-03-11 18:50:18 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,14 +5,12 @@
  * 10 starting from 0, followed by a new line
  * Return: Always 0 (Success)
  */
-
-int main(void)  
+int main(void)
 {
-    int n;
-  
-    for (n = 0; n < 10; n++)
-    
-         printf("%d", n);
-    printf("\n");
-    return (0);  
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
 }
