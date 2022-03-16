6deaff3c7b918b34e6aa5b42800de324e150e608
sodiqmuibi <99649642+sodiqmuibi@users.noreply.github.com>
2022-03-16 12:57:40 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-* times_table - prints tables
-*
-* Return: Always 0.
-*/
+ * times_table -prints tables
+ *
+ * Return: Always 0.
+ */
 void times_table(void)
 {
 int a;
@@ -22,11 +22,26 @@ _putchar ('0');
 }
 if (b != 0)
 {
-_putchar ('0');
+_putchar (' ');
+_putchar ((c % 10) + '0');
 }
 if (b < 9)
 {
-_putchar (',');
+_putchar(',');
+_putchar (' ');
 }
+}
+else
+{
+_putchar ((c / 10) + '0');
+_putchar ((c % 10) + '0');
+if (b < 9)
+{
+_putchar(',');
+_putchar (' ');
+}
+}
+}
+_putchar ('\n');
 }
 }
